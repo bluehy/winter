65baa0fb8730f211a6551d3975e32afe2b8b6005
hy <chessg55@gmail.com>
2019-10-24 16:38:08 +0900

    scss_run

--- f_new_example/public/js/src/main.js
+++ f_new_example/public/js/src/main.js
@@ -9,12 +9,14 @@
    
    // script 내용 간편작성함수 생성
    const JsLink = (temp) => {
-      return body.append(`<script src="../js/src/temp/${temp}"></script>`);
+      return body.append(`<script src="../js/src/temp/${temp}.js"></script>`);
       // return은 공식적으론 써야하지만 안 써도 되긴 함.
    };
 
-   viewBox.load('./temp/slide_01.html',function(){
-      JsLink('slide_01.html');
+   viewBox.load('./temp/slide_01.html',() => {
+                                 // function(){}와 동일한 형태
+      JsLink('slide_01');
    });
+
 
 })(jQuery);

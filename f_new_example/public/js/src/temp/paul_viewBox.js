cd933b67e88f26303a90918c441a09ecd631f851
hy <chessg55@gmail.com>
2019-11-01 14:12:38 +0900

    slide_class_opacity

--- f_new_example/public/js/src/temp/paul_viewBox.js
+++ f_new_example/public/js/src/temp/paul_viewBox.js
@@ -52,19 +52,23 @@ let liLen = slideLi.length;            // == 3
 // li 보이지 않게 하고,  -> li 요소의 첫번째만 보이게 만들고,
 // slideLi.hide();   ->
 slideLi.eq(0).siblings().hide();
+slideLi.eq(0).find('dl').addClass('action');
 
 // -----------------------------------------------------------------------
+
+
+const fakeBtn = $('.fake_btn');
+fakeBtn.hide();
 
 // 버튼 클릭 ----------------------------------------------------------
 
 indiBtn.on('click',function(e){
    e.preventDefault();
-   e.stopPropagation();          // 버블링 현상 제거(크게 효과는X).  (버블링 현상 해소법 1)
-
+   // e.stopPropagation();          // 버블링 현상 제거(크게 효과는X).  (버블링 현상 해소법 1)
+   fakeBtn.show();               // 페이크버튼
 
    // indiBtn.hide();               // 수행이 끝날 때까지 버튼사용을 불가하게 만들기. (버블링 현상 해소법 2)
    // indiBtn.attr({'disable':'true'}); // 버튼 비활성화 (버블링 현상 해소법 3) (잘 안먹힘)
-
 
    if($(this).index() == 0){     //    next 버튼 클릭
       textN++;
@@ -72,12 +76,14 @@ indiBtn.on('click',function(e){
          textN = 0;
       }
       pageNum(textN);
+      indiBtn.eq(0).addClass('action');
    }else{                        //    prev 버튼 클릭
       textN--;
       if(textN <= -1){           // textN이 -1보다 작거나 같아지면, 수행할 것. (페이지 넘버 마지막으로 돌아감)
          textN = liLen - 1;
       }
       pageNum(textN);
+      indiBtn.eq(1).addClass('action');
    }
 
 // 버튼 클릭시 해당하는 순서에 맞는 li 요소를 나타나게 만들기 ------------------
@@ -94,22 +100,28 @@ indiBtn.on('click',function(e){
    //    });//    slideThis.slideDown(function(){});
 
 // ----------------------------------------------------------------------------
-
+   
 // 슬라이드가 늘어나면서 나타나게 만들기-------------------------------------------
 
    let slideThis = slideLi.eq(textN);                             // 변수화
 
-   slideThis.css({'zIndex':liLen + 2, 'width':0, 'overflow':'hidden'});             // 선택된 슬라이드를 강제적으로 위쪽, 크기를 0으로,
-      slideThis.show(function(){                                             // 일단 나타나게 한 후에,
-         slideThis.stop().animate({'width':100 + '%'},1000, 'easeOutCubic' ,function(){     // 크기를 늘리면서 등장, 
+   slideThis.css({'zIndex':liLen + 2, 'width':0, /* 'overflow':'hidden', */'padding':0});             // 선택된 슬라이드를 강제적으로 위쪽, 크기를 0으로,
+      slideThis.css({'display':'block', 'padding':'5vw 0'});                        // 일단 나타나게 한 후에,(패딩값 조절)
+         slideThis.stop().animate({'width':100 + '%','paddingLeft':'5vw'},500, 'easeOutCubic' ,function(){     // 크기를 늘리면서 등장, 
                                                                                     // + (안에 내용물도 등장한 후에 나타나게 하는 것도 방법)
             slideThis.siblings().hide();                                            // 다른 내용들을 지우고,
             slideZindexSet();                                                       // zIndex를 다시 초기화
             
-            indiBtn.show();                                                         // (버블링 현상 해소법 2)
+            // indiBtn.show();                                                         // (버블링 현상 해소법 2)
             // indiBtn.attr({'disable':'false'});                                      //(버블링 현상 해소법 3) (잘 안됨)
+            fakeBtn.hide();
+
+            slideThis.siblings().find('dl').removeClass('action');
+            slideThis.find('dl').addClass('action');
+
+            indiBtn.removeClass('action');
          });//    slideThis.animate({'width':100 + '%'},function(){...}};
-      });//    slideThis.slideDown(function(){...});
+     
 
 // --------------------------------------------------------------------------
 

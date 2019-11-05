5e1d40d15f0005942b921677b5a4a59832a21ca9
hy <chessg55@gmail.com>
2019-11-05 15:22:48 +0900

    indicator_key_setting_focusMove

--- f_new_example/public/js/src/indicator_test_01.js
+++ f_new_example/public/js/src/indicator_test_01.js
@@ -10,11 +10,11 @@
    const productWrap = product.children('ul');
    const productEach = productWrap.children('li');
    
-   
+   productEach.find('a').attr({'tabindex':-1}); // 슬라이드로 링크 이동한후에, 다시 인디케이터 탭으로 돌아올 수 있게.
    indiLi.eq(0).addClass('active');
-
+// ---------------------------------------------------------------
    // 인디케이터 클릭시
-   indiLink.on('focus',function(e){
+   indiLink.on('focus click',function(e){
       e.preventDefault();
       let i = $(this).parent('li').index();
       productWrap.stop().animate({'marginLeft':(-100 * i) + '%'});
@@ -24,13 +24,21 @@
       indiLi.eq(i).siblings('li').removeClass('active');
       // $(this).parent('li').siblings('li').removeClass('active');
 
-      $(this).on('click',function(e){
+      $(this).on('keyup',function(e){
          e.preventDefault();
-         productEach.eq(i).children('a').focus();
+         if(e.keyCode == 13){
+            productEach.eq(i).children('a').focus();
+         }// if 엔터를 쳐야만, 링크로 이동하게끔. 가능하다면, 좌우 키눌렀을 때, 포커스 이동할 수 있게 지정도 해줄 것.
       });
       
    });
 
+   indiLi.on('mouseenter',function(){
+      let i = $(this).index();
+      indiLi.eq(i).css({})
+   })
+
+   // 자동슬라이드 기능
    
    
 })(jQuery);

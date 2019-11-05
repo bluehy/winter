aab23c4063de2db96e1b8974bbfa6ede0184f284
hy <chessg55@gmail.com>
2019-11-05 15:39:56 +0900

    setInterval_clearInterval_autoslidemove

--- f_new_example/public/js/src/indicator_test_01.js
+++ f_new_example/public/js/src/indicator_test_01.js
@@ -10,13 +10,16 @@
    const productWrap = product.children('ul');
    const productEach = productWrap.children('li');
    
+   const slideLen = productEach.length;
+
    productEach.find('a').attr({'tabindex':-1}); // 슬라이드로 링크 이동한후에, 다시 인디케이터 탭으로 돌아올 수 있게.
    indiLi.eq(0).addClass('active');
 // ---------------------------------------------------------------
+   let i = 0;
    // 인디케이터 클릭시
    indiLink.on('focus click',function(e){
       e.preventDefault();
-      let i = $(this).parent('li').index();
+      i = $(this).parent('li').index();
       productWrap.stop().animate({'marginLeft':(-100 * i) + '%'});
 
       indiLi.eq(i).addClass('active');
@@ -33,12 +36,35 @@
       
    });
 
-   indiLi.on('mouseenter',function(){
-      let i = $(this).index();
-      indiLi.eq(i).css({})
-   })
-
    // 자동슬라이드 기능
+   let go, timed =3000; 
+   const GoSlide = function(){
+      go = setInterval(function(){
+         console.log('gogo');
+         i++;
+         if(i >= slideLen){
+            i = 0;
+         };// i의 수치가 배너갯수를 넘어서는 순간, 초기값으로 돌려보냄
+         productWrap.stop().animate({'marginLeft':(-100 * i) + '%'});
+         indiLi.eq(i).addClass('active');
+         indiLi.eq(i).siblings('li').removeClass('active');
+      },timed); // setInterval() 일정시간마다
+
+   };//GoSlide
+   GoSlide();
+
+   // 마우스 올리면 자동슬라이드 멈춤
    
+   const StopSlide = function(){
+      clearInterval(go); // setInterval을 취소하는 기능.
+   }; // StopSlide
    
+   miniProduct.on('mouseenter',function(){
+      StopSlide();
+   }); // miniProduct.on('mouseenter')
+
+   miniProduct.on('mouseleave',function(){
+      GoSlide();
+   });// miniProduct.on('mouseleave')
+
 })(jQuery);

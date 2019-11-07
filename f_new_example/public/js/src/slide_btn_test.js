3dde36fbcc6d2f1f0662f59bc718e7eb1ede28a8
hy <chessg55@gmail.com>
2019-11-07 16:02:43 +0900

    slide>a_bgi_scss_mixin

--- f_new_example/public/js/src/slide_btn_test.js
+++ f_new_example/public/js/src/slide_btn_test.js
@@ -1,4 +1,22 @@
 (function($){
 
+   const product = $('.product');
+   const productUl = product.find('ul');
+   let productLi = productUl.find('li');
+
+   
+   let productLen = productLi.length;
+   for(let i=0;i <productLen;i++){
+      productLi.eq(i).children('a').css({'backgroundImage':'url("../img/slide_test_01/slide_0' + (i+1) + '.png")'});
+   };
+   
+   productLi.eq(-1).clone(true).prependTo(productUl);
+
+   productLi = productUl.find('li');
+   productLen = productLi.length;
+
+   productUl.css({'width':100 * productLen + '%', 'marginLeft':'-100%'});
+   productLi.css({'width':100 / productLen + '%'});
+
 
 })(jQuery);

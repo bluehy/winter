46389f99273f75efa36652513518196fb644b5ca
hy <chessg55@gmail.com>
2019-11-18 16:50:27 +0900

    cbSel_checked_true/false

--- e_example/js/src/join_check.js
+++ e_example/js/src/join_check.js
@@ -39,7 +39,17 @@
 
 
    // 약관 동의 개별 선택, 해제시 전체 동의 해제
-   cbSel.on('click',function(){             
+   cbSel.on('click',function(){
+      // -----------------------------------
+      // 
+      let idCheck = $(this).attr('id');   // 클릭한 cbSel의 id추출
+      let idN = $('#'+idCheck);           // 클릭한 cbSel
+      let idnIs = idN.is(':checked');     // 클릭한 cbSel의 checked상태를 점검. (true/false)
+      (idnIs) ? idN.attr('checked',true).prop('checked',true) : idN.attr('checked',false).prop('checked',false);
+      // 삼항 연산자 ==> (조건) ? 참(true)일 때 수행 : 거짓(false)일 때 수행
+         // ; 이 삼항연산자 중앙에 들어갈 수 X. 
+      
+      // ----------------------------------
       for (let i = 0; i < cbSel.length; i++) {
          // cbSel을 점검할 때, allCheck부분은 포함되지 않도록 조건을 설정해야함
          ck = cbSel.eq(i).is(':checked'); // cbSel.eq(i)들의 checked 상태를 전부 점검

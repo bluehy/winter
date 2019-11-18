706681d1dd4aee2040c44322edb5750affc65e83
hy <chessg55@gmail.com>
2019-11-18 16:03:19 +0900

    allCheck_cbSel_checked_prop&attr

--- e_example/js/src/join_check.js
+++ e_example/js/src/join_check.js
@@ -12,23 +12,59 @@
    // ------------------------------------------------------
    // 전체 동의 클릭시, 전체가 체크되게 만들기
    const allCheck = $('#allCheck');
-   allCheck.on('click',function(e){
-      e.preventDefault();
-      // console.log(e);
-      let thisCheck = $(this).is(':checked');
-      let thisC2 = $(this).prop('checked');
-      let checkAttr = $(this).is('checked');
-      console.log(checkAttr);
+   const cbSel = $('.cb_sel');
+   let ck;
 
-      if (!checkAttr){
-         $(this).attr({'checked':'checked'});
-         // $(this).prop('checked',true);
+   allCheck.on('click',function(e){                // allCheck 클릭
+   //    // e.preventDefault();
+   //    // console.log(e);
+   //    // let thisCheck = $(this).is(':checked');
+   //    // let thisC2 = $(this).prop('checked');
+   //    // let checkAttr = $(this).is('checked');
+      ck = allCheck.is(':checked');                // allCheck의 상태를 체크 = true.
+      console.log(ck);                             // =true
+
+      if (ck){
+         cbSel.prop('checked',true);               //cbSel의 속성 (진짜 속성)
+         cbSel.attr({'checked':true});             //cbSel에 checked 속성이 true (시각적)
+         allCheck.prop('checked', true);
+         allCheck.attr('checked', true);
       }else{
-         $(this).removeAttr('checked');
-         // $(this).prop('checked',false);
-      }
+         cbSel.prop('checked',false);
+         cbSel.attr('checked',false);
+         allCheck.prop('checked', false);
+         allCheck.attr('checked', false);
+      }//if(ck)
    });
 
 
+   // 약관 동의 개별 선택, 해제시 전체 동의 해제
+   cbSel.on('click',function(){             
+      for (let i = 0; i < cbSel.length; i++) {
+         // cbSel을 점검할 때, allCheck부분은 포함되지 않도록 조건을 설정해야함
+         ck = cbSel.eq(i).is(':checked'); // cbSel.eq(i)들의 checked 상태를 전부 점검
+         console.log(ck);                 //
+
+         if(ck == false){                 // cbSel.eq(i) 중 false를 만나게 되면,
+            allCheck.prop('checked',false);  //allCheck에 속성 추가. 
+            allCheck.attr('checked',false);  //
+            break;   // for문의 반복을 끊는 기능. // 그 순간, 점검을 종료.
+                     // (continue = 건너뛰기/ break = 멈추고 빠져나가기)
+         }else{
+            allCheck.prop('checked',true);
+            allCheck.attr('checked',true);//
+         };
+      }// for
+
+      // each사용
+      // cbSel.each(function(data){
+      //    ck = $(this).is(':checked');
+      //    console.log(ck);
+      //    if (ck == false) {
+      //       allCheck.prop('checked', false);
+      //       allCheck.attr('checked', false);
+      //    }
+      // })//위와 동일
+   })//cbSel.on('click');
 
 })(jQuery);
